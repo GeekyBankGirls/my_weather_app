7c0106d779a2af133ec47769614aced9cb6e186f
Wout Kooijman <wapkooijman@gmail.com>
2023-09-21 14:05:33 +0200

    started on TODO for different topics

--- 00_my_first_project.py
+++ 00_my_first_project.py
@@ -1,7 +1,20 @@
-# TODO Mentors: Explain what is PyCharm versus notebook
-
+##############################
+#
+# Welcome!
+# In the comming fields, we will be building a weather app which collects and displays the weather for a cetrain location.
+# We will guide you through the code step by step, if there are any questions then feel free to ask the mentors.
+#
+# Important here is that we will step away from the ease of the Jupyter Notebooks that we used in the 'Intro to Python'
+# This is done because with the '.py' files, which are Python files, we can call them from anywere and it will execute the code and functions specified within.
+# This makes it easier to run bigger applications and more nested structure of code.
+# It may seem daunting to use the terminal to run Python code but we are here to guide you through the process!
+#
+# See this url for more info on terminal versus notebook execution of Python code.
+# https://learnpython.com/blog/python-scripts-vs-jupyter-notebooks/
+#
+##############################
 text = 'Hi, Bella! This is my first weather app!'
 
 print('text')
 
-# TODO TechGirls: Execute th code with the command `python 00_my_first_project.py`
+# TODO TechGirls: Execute th code with the command `python 00_my_first_project.py` in your terminal
--- 01_requests.py
+++ 01_requests.py
@@ -1,4 +1,10 @@
 # TODO Mentors: Explain what is a python package generally and specifically requests & properties
+##############################
+#
+#
+#
+##############################
+
 import requests
 
 url = f'https://www.google.com'
