208e3ff8c0f9dfbf5b4316cb3b7018580e5f31fb
Selin Gungor <selingungor01@gmail.com>
2023-09-27 14:02:34 +0200

    Add functions

--- 03_api_with_dictionary.py
+++ 03_api_with_dictionary.py
@@ -2,28 +2,47 @@
 
 import requests
 
+'''
+Reads:
+    What is json? https://www.w3schools.com/whatis/whatis_json.asp
+    Tips for the exercise: https://stackoverflow.com/questions/59067671/is-there-a-way-to-easily-handle-json-tree-data-in-python
+'''
 
-api_key=""
+api_key="" # add your api key here
 city="Amsterdam"
-url = f'http://api.weather.com/key="{api_key}"+&city="{city}'
-
-# TODO Organizers: will check the parameters from the api & adjust this page
+url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}]&aqi=no'
 
 response = requests.get(url)
 
-# TODO TechGirls:  Get the weather degree for Amsterdam from the weather api for today
+# TODO TechWomen Exercise 1:  Get the weather degree for Amsterdam from the weather api for today
+
+print(response)
 
 if response.status_code == 200:
+    json_response = response.json()
     weather_today = {
-        "city" : response.content["city"],
-        "degree" : response.content["degree"]
+        "city" : json_response["location"]["name"],
+        "degree" : json_response["current"]["temp_c"]
     }
     print(weather_today)
 else:
     print("There was a problem!")
 
 
-# TODO TechGirls: Aren't you curious what's the weather in your home town? Let's check it out.
+# TODO TechWomen Exercise 2: Aren't you curious what's the weather in your home town? Let's find out.
+
+
+# TODO TechWomen Exercise 3: Explore the JSON response from the Weather API and print out the weather condition as a text
+
+if response.status_code == 200:
+    json_response = response.json()
+    weather_today = {
+        "city" : json_response["location"]["name"],
+        "degree" : json_response["current"]["condition"]["text"]
+    }
+    print(weather_today)
+else:
+    print("There was a problem!")
 
 
 
--- 04_api_functions.py
+++ 04_api_functions.py
@@ -1,28 +1,34 @@
 # TODO Mentors: Mentors explain the sequence of the execution with the functions
 
+"""
+Reads:
+    Check out the functions class that we learned in the morning :) https://github.com/TechWomenTribe/introduction_to_python/blob/main/tutorial/02_Functions.ipynb
+"""
+
 import requests
 
-api_key=""
+api_key="" # add your api key here
 city="Amsterdam"
-url = f'http://api.weather.com/key="{api_key}"+&city="{city}'
+url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}]&aqi=no'
 
 response = requests.get(url)
 
-# TODO TechGirls:
-#  1. Add everything in a function that returns the response or the message "There was a problem!"
+# TODO TechWomen Exercise 1:
+#  1. Add everything in a function that returns the city name and the temp_c value or the message "There was a problem!"
 #  2. Call the function
 #  3. Print the result of the function
 
 
 def get_weather():
     if response.status_code == 200:
+        json_response = response.json()
         weather_today = {
-            "city" : response.content["city"],
-            "degree" : response.content["degree"]
+            "city": json_response["location"]["name"],
+            "degree": json_response["current"]["temp_c"]
         }
-        return weather_today
+        print(weather_today)
     else:
-        return "There was a problem!"
+        print("There was a problem!")
 
 
 print(get_weather())
