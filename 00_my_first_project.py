dfbea939adf8c616e3c02188cbcd056d1ffcaf1f
Selin Gungor <selingungor01@gmail.com>
2023-09-27 11:28:37 +0200

    Update first project

--- 00_my_first_project.py
+++ 00_my_first_project.py
@@ -1,8 +1,15 @@
 # TODO Mentors: Explain what is PyCharm versus notebook
-# TODO Organizers: Add a link as a reference
+
+###
+# Reads:
+# What is an IDE? https://www.codecademy.com/article/what-is-an-ide
+# What is PyCharm? https://intellipaat.com/blog/what-is-pycharm/
+###
 
 text = 'Hi, Bella! This is my first weather app!'
 
-print('text')
+print(text)
 
-# TODO TechGirls: Execute the code with the command `python 00_my_first_project.py`
+# TechWomen Exercise 1 : Execute the code with the command `python 00_my_first_project.py`
+
+# TechWomen Exercise 2: Update the text value to something you want to show on the screen and execute the code again.
