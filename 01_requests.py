d3626835d9f010c5a689207eb6408dfacf2bdf46
Wout Kooijman <wapkooijman@gmail.com>
2023-09-29 13:20:26 +0200

    added some initial explanations

--- 01_requests.py
+++ 01_requests.py
@@ -1,9 +1,15 @@
+##############################
+#
+# A Python package is a way of organizing and structuring Python code.
+# It allows you to break down a large programming task into smaller,
+# more manageable subtasks or modules.
+# Packages are structured using “dotted module names” and can contain submodules.
+# They help facilitate modular programming and make it easier to find,
+# install, and share software developed by the Python community.
+#
+##############################
+
 # TODO Mentors: Explain what is a python package generally and specifically requests & properties
-##############################
-#
-#
-#
-##############################
 
 import requests
 
